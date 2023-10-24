9c2c35d8b78208b7c006a9bfe57c2cd31c6f0ea7
Ghislain Leblanc <ghisleb@me.com>
2023-10-24 11:14:22 -0400

    SwiftLint

--- TopUI/TopUI/ContentModel.swift
+++ TopUI/TopUI/ContentModel.swift
@@ -10,7 +10,7 @@ import Foundation
 @MainActor
 class ContentModel: ObservableObject {
     private let myCPUUsage = MyCPUUsage()
-    
+
     var cpuInfo: processor_info_array_t {
         self.myCPUUsage.cpuInfo
     }
--- TopUI/TopUI/ContentView.swift
+++ TopUI/TopUI/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject private var model = ContentModel()
-    
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
