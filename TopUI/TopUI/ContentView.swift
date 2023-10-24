917cd3bc95d086aa292e71161f44034e04e30a67
Ghislain Leblanc <ghisleb@me.com>
2023-10-24 09:13:28 -0400

    Empty project

---
+++ TopUI/TopUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TopUI
+//
+//  Created by Ghislain Leblanc on 2023-10-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TopUI/TopUI/TopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopUIApp.swift
+//  TopUI
+//
+//  Created by Ghislain Leblanc on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct TopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
