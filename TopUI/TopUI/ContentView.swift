52d51a327c6076955c6904e0caf9614ed89b7d54
Ghislain Leblanc <ghisleb@me.com>
2023-10-24 10:31:23 -0400

    Logo

--- TopUI/TopUI/ContentView.swift
+++ TopUI/TopUI/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var model = ContentModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +18,11 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            if let topOutput = model.runTopCommand() {
+                model.parseTopOutput(topOutput)
+            }
+        }
     }
 }
 
